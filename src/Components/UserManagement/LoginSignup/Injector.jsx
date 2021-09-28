9df54e02716e5fa4de73a98aff2ee92ade8a6725
zima nichi <zimanichi@zimas-MacBook-Pro.local>
2021-09-27 19:21:25 -0700

    cors

--- src/Components/UserManagement/LoginSignup/Injector.jsx
+++ src/Components/UserManagement/LoginSignup/Injector.jsx
@@ -1,7 +1,7 @@
 import LoginSignup from "./Controller.jsx"
 import React from "react";
 import ReactDOM from "react-dom";
- async fetch("https://f-server.herokuapp.com/secret", {
+fetch("https://f-server.herokuapp.com/secret", {
             method: 'GET',
             headers: {
               'Accept': 'application/json, text/plain',
@@ -11,7 +11,7 @@ import ReactDOM from "react-dom";
         .then(response=>{return response.json()})
         .then(json=>{
             if(json.credentials){
-                ReactDOM.render(<LoginSignup appCredentials={ await json.credentials}/>, document.getElementById("app"))
+                ReactDOM.render(<LoginSignup appCredentials={ json.credentials}/>, document.getElementById("app"))
             }
 
         })
