aec94eba6ed75c6a193562d8a791b786158d4953
zima nichi <zimanichi@zimas-MacBook-Pro.local>
2021-09-29 19:59:19 -0700

    login with facebook fixed

--- src/Components/UserManagement/Home/Controller.jsx
+++ src/Components/UserManagement/Home/Controller.jsx
@@ -4,11 +4,11 @@ import FlostBar from '../../FlostBar/Controller.jsx';
 export default function Home(props){
     let logout = props.logout
     return(
-        <di id='home-wrapper'>
+        <div id='home-wrapper'>
             <FlostBar user={props.user} accessToken={props.user.accesstoken}/>
             <section>
                 <h1>Check back later for updates!</h1>
-            <section/>
+            </section>
         </div>
     )
 }
