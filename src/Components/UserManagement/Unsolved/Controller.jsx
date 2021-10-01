71c99b4367c5ca57311faeecf9aa5ed1d865cf3d
zima nichi <zimanichi@zimas-MacBook-Pro.local>
2021-10-01 13:51:03 -0700

    make it better

--- src/Components/UserManagement/Unsolved/Controller.jsx
+++ src/Components/UserManagement/Unsolved/Controller.jsx
@@ -6,14 +6,52 @@ function Unsolved(props){
     let [topic, setTopic] = useState(null)
     let [question, setQuestion] = useState(null)
     let login = props.login
+    function removeAlert(){
+        try {
+            document.getElementById('alert').classList.remove('activeAlert')
+        } catch {
+            console.log('ph')
+        }
 
-    function handleSubmit(e){
-        e.stopPropagation()
+    }
+    function handleUnsolved(e){
+    console.log('here')
+        e.preventDefault()
+        try {
+            if (topic && question.length >= 15) {
+                 function Alert(props){
+                    return(
+                        <div>
+                            <p>you must signup/login to upload your question</p>
+                            <button onClick={e=>{props.login(false)}}> get flost </button>
+                            <a href='/'>stay flost</a>
+                        </div>
+                    )
+                 }
+                document.getElementById('alert').className = 'activeAlert'
+                ReactDOM.render(<Alert login={login}/>, document.getElementById('alert'))
+            } else {
+                throw Error
+            }
+        } catch(err){
+            function Alert(props){
+                return(
+                    <div>
+                        <p>question must be at least 15 letters long <br/>you must also select a predefined topic</p>
+                        <button onClick={props.removeAlert}> get flost </button>
+                    </div>
+                )
+            }
+
+            ReactDOM.render(<Alert removeAlert={removeAlert}/>, document.getElementById('alert'))
+            document.getElementById('alert').className = 'activeAlert'
+        }
+
         //post data to the db and toggle unsolved to go back to login/create account
-        login(false)
     }
     return(
     <main>
+        <section id='alert'></section>
         <strong>flost?<br/>get found</strong>
         <section>
             <form>
@@ -26,7 +64,7 @@ function Unsolved(props){
                 </datalist>
                 <textarea onInput={e=>{setQuestion(e.target.value)}} placeholder='should i get the vaccine'></textarea>
                 <div>
-                    <button type='submit' onClick={e=>{handleSubmit(e)}}> continue </button>
+                    <button type='submit' onClick={e=>{handleUnsolved(e)}}> continue </button>
                 </div>
             </form>
         </section>
